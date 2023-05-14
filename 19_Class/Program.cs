c889533b06f989d1bedd051656a18ec8fa2949a6
dbpadhiyar <dbpadhiyar10@outlook.com>
2023-05-14 19:28:12 +0530

    Lecture 20 : Till Static And Instance

---
+++ 19_Class/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+public class Customer
+{
+    string _firstName = string.Empty;
+    string _lastName = string.Empty;
+
+    public Customer() : this("Empty First","Empty Second") // this constructor will be not called if we have parameter construcor and passed value inside it
+    {                                                      // Static constructor will be called only once, no matter how many instance you create
+                                                           // even when we try to access proprty of class static constructor will because it need to initialize static value
+                                                           // Must be parameter less
+        Console.WriteLine("Parameter Less Constructor Called");
+    }
+
+    static Customer()  //Access Modifier not available with static constructor & it will call first
+    {
+        Console.WriteLine("Static Constructor Called");
+    }
+    public Customer(string FirstName, string LastName)
+    {
+        this._firstName = FirstName;
+        this._lastName = LastName;
+    }
+
+    public void PrintFullName()
+    {
+        Console.WriteLine("Parameter Constructor Called \nFull Name is => {0} {1}", this._firstName, this._lastName);
+    }
+}
+class Program
+{  
+    public static void Main()
+    {
+        Customer customer = new Customer("Devendra", "Padhiyar");
+        customer.PrintFullName();
+    }
+}
---
+++ 20_Static_Instance_Class_Member/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Circle
+{
+    static float _PI ; //will be only one copy of _PI no matter how much instance is create
+    int _radius ;
+
+    static Circle()
+    {
+        _PI = 3.14f; //this keyword doesn't work with static keyword, Method, Initializer
+        Console.WriteLine("Static Constructor Called!");
+    }
+
+    public Circle(int radius)
+    {
+        Console.WriteLine("Instance Constructor Called!");
+        this._radius = radius;
+    }
+
+    public float CalculatedArea()
+    {
+        return _PI * this._radius * this._radius;
+    }
+
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Circle c1 = new Circle(5);
+        Console.WriteLine("Circle of 5 is => {0}",c1.CalculatedArea());
+
+        Circle c2 = new Circle(6);
+        Console.WriteLine("Circle of 6 is => {0}", c2.CalculatedArea());
+    }
+}
