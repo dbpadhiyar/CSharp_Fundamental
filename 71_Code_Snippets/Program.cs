83e6128aa2d1d9fbc7e5936edec3b7675d7098c7
dbpadhiyar <dbpadhiyar10@outlook.com>
2023-10-03 22:40:07 +0530

    72 : Dictionary

---
+++ 71_Code_Snippets/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Snippets are one type of shortcut to improve you productivity and get boiler plate syntax
+ * snippets like Insert snippets (Ctrl+K,X)
+ * snippets like Surround with (Ctrl+K,S) are useful to speed up our productivity
+ * we can add snippets using code snippets manager
+ */ 
+class Program
+{
+    public static void Main()
+    {
+        List<int> list = new List<int>() { 10,20,30,44,55,66,77,88,99,100};
+        for (int i = 0; i < 5; i++)
+        {
+            for (int l = 0; l < list.Count; l++)
+            {
+                Console.WriteLine(list[l]);
+
+            } 
+        }
+
+    }
+}
---
+++ 72_Dictionary/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Dictionary is one type of datatype which store value in key value pair
+ * Dictionary contail 2 form of day 1st is key and 2nd is value
+ * Key must be unique inside Dictionary
+ * Dictionary provides fast lookups for value using keys
+ */ 
+class Program
+{
+    public static void Main()
+    {
+        Customer customer = new Customer()
+        {
+            Id = 1,
+            Name = "Devendra",
+            Salary = 100
+        };
+
+        Customer customer2 = new Customer()
+        {
+            Id = 2,
+            Name = "Vinit",
+            Salary = 200
+        };
+
+        Customer customer3 = new Customer()
+        {
+            Id = 3,
+            Name = "Krrish",
+            Salary = 300
+        };
+
+
+        Dictionary<int, Customer> customerDictionary = new Dictionary<int, Customer>();
+        customerDictionary.Add(customer.Id, customer);
+        customerDictionary.Add(customer2.Id, customer2);
+        customerDictionary.Add(customer3.Id, customer3);
+
+        if(customerDictionary.ContainsKey(customer.Id)) // to check whether key is already exist in dictionary or not
+        {
+            customerDictionary.Add(customer.Id,customer);
+        }
+
+        Customer findCustomer = customerDictionary[3];
+        Console.WriteLine($"Name of customer is {findCustomer.Name} and salary is {findCustomer.Salary}");
+
+
+        foreach (KeyValuePair<int,Customer> customers in customerDictionary)
+        {
+            Console.WriteLine($"ID of customer is {customers.Key}");
+            Customer individualCustomer = customers.Value;
+            Console.WriteLine($"ID is {individualCustomer.Id} Name is {individualCustomer.Name} and salary is {individualCustomer.Salary}");
+        }
+
+    }
+}
+
+public class Customer
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Salary { get; set; }
+}
